623ad91ce1184bcc7efbaf016474f5b93d3d38e0
Randy Taylor <tehgecKozzz@gmail.com>
2016-12-26 22:07:14 -0500

    Modify folder structure :art: Tabs -> Spaces

---
+++ src/MenuState.cpp
@@ -0,0 +1,51 @@
+#include <SFML/Window/Event.hpp>
+#include <iostream>
+
+#include "MenuState.hpp"
+#include "GameState.hpp"
+
+MenuState::MenuState(StateManager& stateManager, sf::RenderWindow& window)
+    : State{ stateManager, window } {
+    std::cout << "[MenuState] Initialized" << std::endl;
+}
+
+void MenuState::pause() {
+
+}
+
+void MenuState::resume() {
+
+}
+
+void MenuState::processEvents() {
+    sf::Event event;
+
+    while (m_window.pollEvent(event)) {
+        switch (event.type) {
+            case sf::Event::Closed:
+                m_stateManager.quit();
+                break;
+            case sf::Event::KeyPressed:
+                switch (event.key.code) {
+                    case sf::Keyboard::Space:
+                    {
+                        std::unique_ptr<State> gameState(new GameState(m_stateManager, m_window));
+                        m_stateManager.stateToChangeTo(std::move(gameState));
+                        break;
+                    }
+                    case sf::Keyboard::Escape:
+                        m_stateManager.quit();
+                        break;
+                }
+        }
+    }
+}
+
+void MenuState::update(const sf::Time& dt) {
+
+}
+
+void MenuState::draw() {
+    m_window.clear(sf::Color::Black);
+    m_window.display();
+}
---
+++ src/StateManager.hpp
@@ -0,0 +1,33 @@
+#pragma once
+#include <stack>
+#include <memory>
+
+#include "State.hpp"
+
+class StateManager {
+public:
+    StateManager();
+    ~StateManager();
+
+    void pushState(std::unique_ptr<State> state);
+    void popState();
+    void changeState(std::unique_ptr<State> state);
+
+    void processEvents();
+    void update(sf::Time dt);
+    void draw();
+
+    void stateToChangeTo(std::unique_ptr<State> state);
+    std::unique_ptr<State> getCurrentState()                { return std::move(m_states.top()); }
+
+    bool running()                                          { return m_running; }
+    void quit()                                             { m_running = false; }
+
+    bool isChangingState()                                  { return m_changingState; }
+    std::unique_ptr<State> getNewState()                    { return std::move(m_newState); }
+private:
+    std::stack<std::unique_ptr<State>> m_states;
+    std::unique_ptr<State> m_newState;
+    bool m_running;
+    bool m_changingState;
+};
