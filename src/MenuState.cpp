d2b4ed8ce14523ffcc37ade82c8cd399c8b9baf7
Marcus Bieber <@arcuss-MBP.fritz.box>
2019-05-14 09:12:37 +0200

    Fix for changeState Event in loop

--- src/MenuState.cpp
+++ src/MenuState.cpp
@@ -34,7 +34,7 @@ void MenuState::processEvents() {
                     case sf::Keyboard::Space: {
                         std::unique_ptr<State> gameState(new GameState(m_stateManager, m_window));
                         m_stateManager.changeState(std::move(gameState));
-                        break;
+                        return;
                     }
                     case sf::Keyboard::Escape:
                         m_stateManager.quit();
